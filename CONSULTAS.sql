6ff5fb1fd71a7895374b99bec584bb94828fd007
Eric Bravo Padilla <49547727+Ericsan98@users.noreply.github.com>
2021-12-08 15:17:54 -0600

    Códigos
    Generación de fragmentos y consultas

---
+++ CONSULTAS.sql
@@ -0,0 +1,337 @@
+--CONSULTAS
+
+--1. Obtener los 3 primeros empleados que han tenido las mayores ventas totales el año pasado.
+--(tablas implicadas person, empleyee, salesPerson[añadir a grafo])
+
+create procedure Top3_vendedores_mayores_ventas_LastYear_por_fragmento @nom_fragmento varchar(100) as
+begin
+	declare @servidor nvarchar(100);
+	declare @sql nvarchar(1000);
+
+	-- Recuperación del nombre del servidor vinculado y BD del fragmento
+	select @servidor = servidor
+	from diccionario_distribucion
+	where bd = @nom_fragmento;
+
+	set @sql = 'insert into Top3_Vendedores_mas_ventas_LastYear (FirstName, MiddleName, LastName, SalesLastYear)
+				select P.FirstName, P.MiddleName, P.LastName, SP.SalesLastYear
+				from ' + @servidor + '.' + @nom_fragmento + '.dbo.SalesPerson SP join ' + @servidor + '.' + @nom_fragmento + '.dbo.Employee E
+				on SP.BusinessEntityID = E.BusinessEntityID
+				join ' + @servidor + '.' + @nom_fragmento + '.dbo.Person P on P.BusinessEntityID = E.BusinessEntityID';
+	exec sp_executesql @sql;
+end
+Go
+
+create procedure Top3_vendedores_mayores_ventas_LastYear as
+begin
+	
+	create table Top3_Vendedores_mas_ventas_LastYear(
+		FirstName nvarchar(50),
+		MiddleName nvarchar(50),
+		LastName nvarchar(50),
+		SalesLastYear money
+	)
+
+	-- cursor ----------------------------------------------------
+	DECLARE @nombre_fragmento AS nvarchar(100)
+	DECLARE fragmento CURSOR FOR SELECT bd FROM diccionario_distribucion
+	OPEN fragmento
+	FETCH NEXT FROM fragmento INTO @nombre_fragmento
+	WHILE @@fetch_status = 0
+	BEGIN
+		exec Top3_vendedores_mayores_ventas_LastYear_por_fragmento @nombre_fragmento;
+		FETCH NEXT FROM fragmento INTO @nombre_fragmento
+	END
+	CLOSE fragmento
+	DEALLOCATE fragmento
+
+	select top 3 * from Top3_Vendedores_mas_ventas_LastYear order by SalesLastYear desc
+	
+	drop table Top3_Vendedores_mas_ventas_LastYear
+end
+go
+
+exec Top3_vendedores_mayores_ventas_LastYear
+GO
+
+
+
+--2. La cantidad de ventas totales por territorio. 
+create procedure Contando_Ventas_por_territorio @nom_fragmento varchar(100) as
+begin
+	declare @servidor nvarchar(100);
+	declare @sql nvarchar(1000);
+
+	-- Recuperación del nombre del servidor vinculado y BD del fragmento
+	select @servidor = servidor
+	from diccionario_distribucion
+	where bd = @nom_fragmento;
+
+	set @sql = 'insert into Cuentas_por_Territorio (Territorio, Ventas_Totales)
+				select ST.[name], SOH.Ventas_Totales from
+				(select TerritoryID, count(*) as Ventas_Totales from ' + @servidor + '.' + @nom_fragmento + '.dbo.SalesOrderHeader
+				group by TerritoryID) as SOH
+				join ' + @servidor + '.' + @nom_fragmento + '.dbo.SalesTerritory ST on ST.TerritoryID = SOH.TerritoryID';
+	exec sp_executesql @sql;
+end
+Go
+
+create procedure Total_ventas_por_territorio as
+begin
+	
+	create table Cuentas_por_Territorio(
+		Territorio nvarchar(50),
+		Ventas_Totales int
+	)
+
+	-- cursor ----------------------------------------------------
+	DECLARE @nombre_fragmento AS nvarchar(100)
+	DECLARE fragmento CURSOR FOR SELECT bd FROM diccionario_distribucion
+	OPEN fragmento
+	FETCH NEXT FROM fragmento INTO @nombre_fragmento
+	WHILE @@fetch_status = 0
+	BEGIN
+		exec Contando_Ventas_por_territorio @nombre_fragmento;
+		FETCH NEXT FROM fragmento INTO @nombre_fragmento
+	END
+	CLOSE fragmento
+	DEALLOCATE fragmento
+
+	select Territorio, sum(Ventas_Totales) Ventas_Totales from Cuentas_por_Territorio group by Territorio
+	
+	drop table Cuentas_por_Territorio
+end
+go
+
+exec Total_ventas_por_territorio
+go
+
+
+
+--3. Contar las órdenes de venta por tipo de envío
+create procedure Contando_Ventas_por_metodo_envio @nom_fragmento varchar(100) as
+begin
+	declare @servidor nvarchar(100);
+	declare @sql nvarchar(1000);
+
+	-- Recuperación del nombre del servidor vinculado y BD del fragmento
+	select @servidor = servidor
+	from diccionario_distribucion
+	where bd = @nom_fragmento;
+
+	set @sql = 'insert into Cuentas_por_metodo_envio ([Metodo_envio], Ventas_Totales)
+				select [name], SOH.Ventas_Totales from
+				(select ShipMethodID, count(*) as Ventas_Totales from ' + @servidor + '.' + @nom_fragmento + '.dbo.SalesOrderHeader
+				group by ShipMethodID) SOH
+				join ' + @servidor + '.' + @nom_fragmento + '.dbo.ShipMethod SM on SM.ShipMethodID = SOH.ShipMethodID';
+	exec sp_executesql @sql;
+end
+Go
+
+create procedure Total_ventas_por_metodo_envio as
+begin
+	
+	create table Cuentas_por_metodo_envio(
+		Metodo_envio nvarchar(50),
+		Ventas_Totales int
+	)
+
+	-- cursor ----------------------------------------------------
+	DECLARE @nombre_fragmento AS nvarchar(100)
+	DECLARE fragmento CURSOR FOR SELECT bd FROM diccionario_distribucion
+	OPEN fragmento
+	FETCH NEXT FROM fragmento INTO @nombre_fragmento
+	WHILE @@fetch_status = 0
+	BEGIN
+		exec Contando_Ventas_por_metodo_envio @nombre_fragmento;
+		FETCH NEXT FROM fragmento INTO @nombre_fragmento
+	END
+	CLOSE fragmento
+	DEALLOCATE fragmento
+
+	select Metodo_envio, sum(Ventas_Totales) Ventas_Totales from Cuentas_por_metodo_envio group by Metodo_envio
+	
+	drop table Cuentas_por_metodo_envio
+end
+go
+
+exec Total_ventas_por_metodo_envio
+go
+
+
+
+--4. Eliminar la última orden de venta registrada
+create procedure Eliminacion_ultima_orden_venta as
+begin
+	declare @SalesOrderID_MAX int;
+	declare @Fragmento nvarchar(100);
+	declare @Parametros nvarchar(1000);
+	declare @sql nvarchar(1000);
+	declare @servidor_volatil nvarchar(100);
+	declare @servidor nvarchar(100);
+	set @SalesOrderID_MAX = 0;
+
+	-- cursor ----------------------------------------------------
+	DECLARE @nom_fragmento AS nvarchar(100)
+	DECLARE fragmento CURSOR FOR SELECT bd FROM diccionario_distribucion
+	OPEN fragmento
+	FETCH NEXT FROM fragmento INTO @nom_fragmento
+	WHILE @@fetch_status = 0
+	BEGIN
+		select @servidor_volatil = servidor
+		from diccionario_distribucion where bd = @nom_fragmento;
+
+		set @sql = 'if ((select max(SalesOrderID) from ' + @servidor_volatil + '.' + @nom_fragmento + '.dbo.SalesOrderHeader) > ' + cast(@SalesOrderID_MAX as nvarchar(10)) + ')
+						begin
+							select @SalesOrderID_MAX = max(SalesOrderID) from ' + @servidor_volatil + '.' + @nom_fragmento + '.dbo.SalesOrderHeader
+							set @servidor = ' + @servidor_volatil + '
+							set @Fragmento = ' + @nom_fragmento + '
+						end
+					else
+					 begin
+						set @SalesOrderID_MAX = ' + cast(@SalesOrderID_MAX as nvarchar(10)) + '
+						set @servidor = ' + @servidor + '
+						set @Fragmento = ' + @Fragmento + '
+					 end'
+		set @Parametros = '@servidor nvarchar(100) OUTPUT, @SalesOrderID_MAX int OUTPUT, @Fragmento nvarchar(100) OUTPUT';
+		exec sp_executesql @sql,@Parametros,@servidor = @servidor output, @SalesOrderID_MAX = @SalesOrderID_MAX output,
+			@Fragmento = @Fragmento output;
+
+		FETCH NEXT FROM fragmento INTO @nom_fragmento
+	END
+	CLOSE fragmento
+	DEALLOCATE fragmento
+
+	set @sql = 'delete from ' + @servidor + '.' + @Fragmento + '.dbo.SalesOrderHeader where SalesOrderID = ' + cast(@SalesOrderID_MAX as nvarchar(10))
+	exec sp_executesql @sql;
+	
+end
+go
+
+
+
+--5. Listar los vendedores asignados a cada Tienda cliente 
+create procedure Lista_Tiendas_Vendedores as
+begin
+	declare @sql nvarchar(1000);
+	declare @servidor nvarchar(100);
+
+	create table Tienda_Vendedores(
+		TIENDA nvarchar(50),
+		FirstName_Vendedor nvarchar(50),
+		MiddleName_Vendedor nvarchar(50),
+		LastName_Vendedor nvarchar(50)
+	)
+
+	-- cursor ----------------------------------------------------
+	DECLARE @nom_fragmento AS nvarchar(100)
+	DECLARE fragmento CURSOR FOR SELECT bd FROM diccionario_distribucion
+	OPEN fragmento
+	FETCH NEXT FROM fragmento INTO @nom_fragmento
+	WHILE @@fetch_status = 0
+	BEGIN
+		select @servidor = servidor
+		from diccionario_distribucion where bd = @nom_fragmento;
+
+		set @sql = 'insert into Tienda_Vendedores
+					select S.[name], FirstName, MiddleName, LastName from ' + @servidor + '.' + @nom_fragmento + '.dbo.Store S
+					join ' + @servidor + '.' + @nom_fragmento + '.dbo.Person P on S.SalesPersonID = P.BusinessEntityID';
+		exec sp_executesql @sql;
+
+		FETCH NEXT FROM fragmento INTO @nom_fragmento
+	END
+	CLOSE fragmento
+	DEALLOCATE fragmento
+
+	select * from Tienda_Vendedores order by TIENDA
+	drop table Tienda_Vendedores
+	
+end
+go
+
+exec Lista_Tiendas_Vendedores
+GO
+
+
+
+--6. Obtener la cantidad de órdenes de venta por vendedor 
+alter procedure Total_ventas_por_vendedor as
+begin
+
+	create table Cuentas_por_Vendedor(
+		FirstName_Vendedor nvarchar(50),
+		MiddleName_Vendedor nvarchar(50),
+		LastName_Vendedor nvarchar(50),
+		Ventas_Totales int
+	)
+
+	insert into Cuentas_por_Vendedor
+	select FirstName, MiddleName, LastName, count(*) from SalesOrderHeader SOH
+	join Person P on SOH.SalesPersonID = P.BusinessEntityID
+	GROUP BY FirstName, MiddleName, LastName
+				
+	insert into Cuentas_por_Vendedor
+	select FirstName, MiddleName, LastName, count(*) from [ConnectionFragmeto1a2].Fragmento2AW.dbo.SalesOrderHeader SOH
+	join Person P on SOH.SalesPersonID = P.BusinessEntityID
+	GROUP BY FirstName, MiddleName, LastName
+				
+	insert into Cuentas_por_Vendedor
+	select FirstName, MiddleName, LastName, count(*) from SalesOrderHeader SOH
+	join [ConnectionFragmeto1a2].Fragmento2AW.dbo.Person P
+	on SOH.SalesPersonID = P.BusinessEntityID
+	GROUP BY FirstName, MiddleName, LastName
+				
+	insert into Cuentas_por_Vendedor
+	select FirstName, MiddleName, LastName, count(*) from [ConnectionFragmeto1a2].Fragmento2AW.dbo.SalesOrderHeader SOH
+	join [ConnectionFragmeto1a2].Fragmento2AW.dbo.Person P
+	on SOH.SalesPersonID = P.BusinessEntityID
+	GROUP BY FirstName, MiddleName, LastName
+
+	select FirstName_Vendedor, MiddleName_Vendedor, LastName_Vendedor, sum(Ventas_Totales) from Cuentas_por_Vendedor
+	group by FirstName_Vendedor, MiddleName_Vendedor, LastName_Vendedor
+	
+	drop table Cuentas_por_Vendedor
+end
+go
+
+exec Total_ventas_por_vendedor
+go
+
+
+
+--7. Obtener los clientes que han comprado asociados a que están recibiendo publicidad por correo electrónico y su correspondiente total de
+--ordenes de venta 
+select P.FirstName, P.MiddleName, P.LastName, count(*) from [ConnectionFragmeto2a1].Fragmento1AW.dbo.SalesOrderHeader SOH
+join [ConnectionFragmeto2a1].Fragmento1AW.dbo.Customer C on SOH.CustomerID = C.CustomerID
+join [ConnectionFragmeto2a1].Fragmento1AW.dbo.Person P on C.PersonID = P.BusinessEntityID
+group by P.FirstName, P.MiddleName, P.LastName
+
+
+
+--8. Obtener los tipos de razones de ventas de los clientes suscritos a publicidad por correo electrónico y
+--su correspondiente total de ordenes de venta 
+select ReasonType, count(*) from [ConnectionFragmeto2a1].Fragmento1AW.dbo.SalesReason SR
+join [ConnectionFragmeto2a1].Fragmento1AW.dbo.SalesOrderHeaderSalesReason SOHSR on SR.SalesReasonID = SOHSR.SalesReasonID
+group by ReasonType
+
+
+
+--9. Obtener las razones de venta de los clientes no suscritos a publicidad por correo electrónico y
+--su correspondiente total de ordenes de venta 
+select [name], count(*) from [ConnectionFragmeto1a2].Fragmento2AW.dbo.SalesReason SR
+join [ConnectionFragmeto1a2].Fragmento2AW.dbo.SalesOrderHeaderSalesReason SOHSR on SR.SalesReasonID = SOHSR.SalesReasonID
+group by [name]
+
+
+
+
+--Creación de Índices no agrupados
+create nonclustered index IX_Nonclustered_Person_CompleteName
+on dbo.Person (BusinessEntityID)
+include (FirstName, MiddleName, LastName)
+
+create nonclustered index IX_Nonclustered_SalesPerson_SalesLY
+on SalesPerson (BusinessEntityID)
+include (SalesLastYear)
+
+drop index SalesPerson.[IX_Nonclustered_SalesPerson_SalesLY]
---
+++ Operaciones_Fragmento1.sql
@@ -0,0 +1,350 @@
+---------------------------------FRAGMENTO 1 (EmailPromotion = 0)
+-- CREACIÓN DE TABLAS
+create database Fragmento1AW;
+go
+
+use Fragmento1AW;
+go
+
+-- CREACIÓN DE TABLAS
+create table Person(
+	BusinessEntityID int,
+	PersonType nchar(2),
+	NameStyle bit,
+	Title nvarchar(8),
+	FirstName nvarchar(50),
+	MiddleName nvarchar(50),
+	LastName nvarchar(50),
+	Suffix nvarchar(10),
+	EmailPromotion int,
+	rowguid uniqueidentifier,
+	ModifiedDate datetime,
+	constraint PK_Person_BusinessEntityID primary key (BusinessEntityID)
+);
+GO
+
+create table SalesTerritory(
+	TerritoryID int identity(1,1),
+	[Name] nvarchar(50),
+	CountryRegionCode nvarchar(3), 
+	[Group] nvarchar(50),
+	SalesYTD money,
+	SalesLastYear money,
+	CostYTD money,
+	CostLastYear money,
+	rowguid uniqueidentifier,
+	ModifiedDate datetime
+	constraint PK_SalesTerritory_TerritoryID primary key (TerritoryID)
+);
+GO
+
+create table Customer(
+	CustomerID int,
+	PersonID int,
+	StoreID int,
+	TerritoryID int,
+	AccountNumber varchar(10),
+	rowguid uniqueidentifier,
+	ModifiedDate datetime
+	constraint PK_Customer_CustomerID primary key (CustomerID)
+	CONSTRAINT FK_Customer_Person_PersonID
+	FOREIGN KEY (PersonID)
+	REFERENCES dbo.Person(BusinessEntityID),
+	CONSTRAINT FK_Customer_SalesTerritory_TerritoryID
+	FOREIGN KEY (TerritoryID)
+	REFERENCES dbo.SalesTerritory(TerritoryID)
+);
+GO
+
+create table Employee(
+	BusinessEntityID int,
+	NationalIDNumber nvarchar(15),
+	LoginID nvarchar(256),
+	OrganizationLevel smallint,
+	JobTitle nvarchar(50),
+	BirthDate date,
+	MaritalStatus nchar(1),
+	Gender nchar(15),
+	HireDate date,
+	SalariedFlag bit,
+	VacationHours smallint,
+	SickLeaveHours smallint,
+	CurrentFlag bit,
+	rowguid uniqueidentifier,
+	ModifiedDate datetime,
+	constraint PK_Employee_BusinessEntityID primary key (BusinessEntityID),
+	CONSTRAINT FK_Employee_Person_BusinessEntityID
+	FOREIGN KEY (BusinessEntityID)
+	REFERENCES dbo.Person(BusinessEntityID)
+);
+GO
+
+create table SalesPerson(
+	BusinessEntityID int,
+	TerritoryID int,
+	SalesQuota money,
+	Bonus money,
+	CommissionPct smallmoney,
+	SalesYTD money,
+	SalesLastYear money,
+	rowguid uniqueidentifier,
+	ModifiedDate datetime
+	constraint PK_SalesPerson_BusinessEntityID primary key (BusinessEntityID),
+	CONSTRAINT FK_SalesPerson_Employee_BusinessEntityID
+	FOREIGN KEY (BusinessEntityID)
+	REFERENCES Employee(BusinessEntityID),
+	CONSTRAINT FK_SalesPerson_SalesTerritory_TerritoryID
+	FOREIGN KEY (TerritoryID)
+	REFERENCES SalesTerritory(TerritoryID)
+);
+GO
+
+create table Store(
+	BusinessEntityID int,
+	[Name] nvarchar(50),
+	SalesPersonID int,
+	rowguid uniqueidentifier,
+	ModifiedDate datetime,
+	constraint PK_Store_BusinessEntityID primary key (BusinessEntityID),
+	CONSTRAINT FK_Store_SalesPerson_SalesPersonID
+	FOREIGN KEY (SalesPersonID) 
+	REFERENCES SalesPerson(BusinessEntityID),
+);
+GO
+
+create table ShipMethod(
+	ShipMethodID int,
+	[Name] nvarchar(50),
+	ShipBase money,
+	ShipRate money,
+	rowguid uniqueidentifier,
+	ModifiedDate datetime
+	constraint PK_ShipMethod_ShipMethodID primary key (ShipMethodID)
+)
+GO
+
+create table SalesOrderHeader(
+    SalesOrderID int,
+    RevisionNumber tinyint,
+    OrderDate datetime,
+    DueDate datetime,
+    ShipDate datetime,
+    [Status] tinyint,
+    OnlineOrderFlag bit,
+    SalesOrderNumber nvarchar(25),
+    PurchaseOrderNumber nvarchar(25),
+    AccountNumber nvarchar(25),
+    CustomerID int,
+    SalesPersonID int,
+    TerritoryID int,
+    BillToAddressID int,
+    ShipToAddressID int,
+    ShipMethodID int,
+    CreditCardID int,
+    CreditCardApprovalCode varchar(15), 
+    CurrencyRateID int,
+    SubTotal money,
+    TaxAmt money,
+    Freight money,
+    TotalDue money,
+    Comment nvarchar(128),
+    rowguid uniqueidentifier,
+    ModifiedDate datetime,
+    constraint PK_SalesOrderHeader_SalesOrderID primary key (SalesOrderID),
+	CONSTRAINT FK_SalesOrderHeader_Customer_CustomerID
+	FOREIGN KEY (CustomerID)
+	REFERENCES dbo.Customer(CustomerID),
+	CONSTRAINT FK_SalesOrderHeader_SalesTerritory_TerritoryID
+	FOREIGN KEY (TerritoryID)
+	REFERENCES SalesTerritory(TerritoryID),
+	CONSTRAINT FK_SalesOrderHeader_ShipMethod_ShipMethodID
+	FOREIGN KEY (ShipMethodID)
+	REFERENCES ShipMethod(ShipMethodID)
+);
+GO
+
+create table SalesReason(
+	SalesReasonID int,
+	[Name] nvarchar(50),
+	ReasonType nvarchar(50),
+	ModifiedDate datetime
+	constraint PK_SalesReason_SalesReasonID primary key (SalesReasonID)
+)
+GO
+
+create table SalesOrderHeaderSalesReason(
+	SalesOrderID int, 
+	SalesReasonID int,
+	ModifiedDate datetime,
+	constraint PK_SalesOrderHeaderSalesReason_SalesOrderID_SalesReasonID primary key (SalesOrderID, SalesReasonID),
+	CONSTRAINT FK_SalesOrderHeaderSalesReason_SalesOrderHeader_SalesOrderID
+	FOREIGN KEY (SalesOrderID)
+	REFERENCES SalesOrderHeader(SalesOrderID) on delete cascade,
+	CONSTRAINT FK_SalesOrderHeaderSalesReason_SalesReason_SalesReasonID
+	FOREIGN KEY (SalesReasonID)
+	REFERENCES SalesReason(SalesReasonID)
+)
+GO
+
+
+--CREACIÓN DE SERVIDOR VINCULADO QUE CONECTARÁ CON EL SEGUNDO FRAGMENTO
+EXEC sp_addlinkedserver
+   @server=N'ConnectionFragmeto1a2',
+   @srvproduct=N'SQLServer',
+   @provider=N'SQLOLEDB', -- Microsoft OLE DB Provider for SQL Server
+   @datasrc=N'192.168.100.39\INSTANCIA2';
+
+
+EXEC sp_addlinkedsrvlogin
+    @rmtsrvname = N'ConnectionFragmeto1a2',
+    @useself = N'true', -- Se conecta con al servidor remoto con las credenciales actuales del servidor local
+    @locallogin = NULL; -- Se establece en NULL para que se vean afectados todos los inicios de sesión locales
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'Collation Compatible',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'Data Access',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'RPC',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'RPC out',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'Use Remote Collation',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'remote proc transaction promotion',
+    -- Enable Promotion of Distributed Transactions for RPC
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'Dist', --Distributor
+    @optvalue = 'false';
+
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'Pub', --publisher
+    @optvalue = 'false';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'sub', -- subscriber
+    @optvalue = 'false';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto1a2',
+    @optname = 'Lazy Schema Validation',
+    @optvalue = 'false';
+
+GO
+
+
+--REDUNDANCIA (Servidor vinculado que apunta a si mismo)
+--CREACIÓN DE SERVIDOR VINCULADO QUE CONECTARÁ CON EL PRIMER FRAGMENTO
+EXEC sp_addlinkedserver
+   @server=N'ConnectionFragmeto2a1',
+   @srvproduct=N'SQLServer',
+   @provider=N'SQLOLEDB', -- Microsoft OLE DB Provider for SQL Server
+   @datasrc=N'192.168.100.39\INSTANCIA1';
+
+
+EXEC sp_addlinkedsrvlogin
+    @rmtsrvname = N'ConnectionFragmeto2a1',
+    @useself = N'true', -- Se conecta con al servidor remoto con las credenciales actuales del servidor local
+    @locallogin = NULL; -- Se establece en NULL para que se vean afectados todos los inicios de sesión locales
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'Collation Compatible',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'Data Access',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'RPC',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'RPC out',
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'Use Remote Collation',
+    @optvalue = 'true';
+
+EXEC sp_serveroption 
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'remote proc transaction promotion',
+    -- Enable Promotion of Distributed Transactions for RPC
+    @optvalue = 'true';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'Dist', --Distributor
+    @optvalue = 'false';
+
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'Pub', --publisher
+    @optvalue = 'false';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'sub', -- subscriber
+    @optvalue = 'false';
+
+EXEC sp_serveroption
+    @server = 'ConnectionFragmeto2a1',
+    @optname = 'Lazy Schema Validation',
+    @optvalue = 'false';
+GO
+
+
+
+-- DICCIONARIO DE DISTRIBUCIÓN
+CREATE TABLE diccionario_distribucion (
+  id_fragmento tinyint primary key, -- identificador del fragmento
+  servidor varchar(100), -- nombre del servidor vinculado
+  bd varchar(100), -- nombre de la base que aloja al fragmento
+  col_frag varchar(100) -- columna que se utiliza como criterio de fragmentación
+)
+GO
+
+-- Tuplas del diccionario
+insert into diccionario_distribucion values (1,'ConnectionFragmeto2a1','Fragmento1AW','EmailPromotion');  -- Instancia 1 SQL Server
+insert into diccionario_distribucion values (2,'ConnectionFragmeto1a2','Fragmento2AW','EmailPromotion'); -- Instancia 2 SQL Server
+go
+
+-- Tabla de valores que toma el atributo que se consideró para la fragmentación
+CREATE TABLE val_email_promotion (
+  id_fragmento tinyint,
+  val_col varchar(100),
+  primary key (id_fragmento, val_col),
+  foreign KEY (id_fragmento) REFERENCES diccionario_distribucion (id_fragmento)
+)
+go
+
+insert into val_email_promotion values (1,'0')
